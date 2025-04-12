f6e0b5b4dc5aa85631c20c5c5b99be03776e6eeb
Deepika-Garg <garg.deepika21@gmail.com>
2025-04-12 21:51:44 +0100

    completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-    
+
     document.getElementById("answer-box").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             checkAnswer();
@@ -47,6 +47,10 @@ function runGame(gameType) {
     else if (gameType==="subtract")
         {
             displaySubtractQuestion(num1,num2); 
+        }
+    else if (gameType==="division")
+        {
+                displayDivisionQuestion(num1,num2); 
         }
     else{
         alert(`Unknown game type ${gameType}`);
@@ -101,6 +105,11 @@ function calculateCorrectAnswer() {
                 return [operand1 - operand2, "subtract"];
         
             } 
+            else  if(operator==="/")
+                {
+                    return [operand1 / operand2, "division"];
+            
+                } 
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -147,4 +156,11 @@ function displayMultiplyQuestion(operand1,operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 
+}
+
+function displayDivisionQuestion(operand1,operand2) {
+
+    document.getElementById('operand1').textContent = operand1*operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
